ee0273a84e466fc1bd22b22a22c3f5ecedea9c20
Code-Godkiller <zggdscutlyh@163.com>
2017-09-27 20:50:39 +0800

    origin

---
+++ RandomSystem/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.开始SToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.退出EToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.功能选择SToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.随机生成评委ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.帮助HToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.重启RToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(392, 146)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(372, 373)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "随机生成评委"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("黑体", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 60
+        Me.ListBox1.Location = New System.Drawing.Point(10, 35)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(9)
+        Me.ListBox1.MultiColumn = True
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(362, 484)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("黑体", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.IntegralHeight = False
+        Me.ComboBox1.ItemHeight = 25
+        Me.ComboBox1.Location = New System.Drawing.Point(392, 101)
+        Me.ComboBox1.MaxDropDownItems = 15
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(372, 33)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(384, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(379, 43)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "请选择演讲者姓名"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.开始SToolStripMenuItem, Me.功能选择SToolStripMenuItem, Me.帮助HToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(782, 28)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '开始SToolStripMenuItem
+        '
+        Me.开始SToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.重启RToolStripMenuItem, Me.退出EToolStripMenuItem})
+        Me.开始SToolStripMenuItem.Name = "开始SToolStripMenuItem"
+        Me.开始SToolStripMenuItem.Size = New System.Drawing.Size(70, 24)
+        Me.开始SToolStripMenuItem.Text = "开始(&S)"
+        '
+        '退出EToolStripMenuItem
+        '
+        Me.退出EToolStripMenuItem.Name = "退出EToolStripMenuItem"
+        Me.退出EToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.退出EToolStripMenuItem.Text = "退出(&E)"
+        '
+        '功能选择SToolStripMenuItem
+        '
+        Me.功能选择SToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.随机生成评委ToolStripMenuItem})
+        Me.功能选择SToolStripMenuItem.Name = "功能选择SToolStripMenuItem"
+        Me.功能选择SToolStripMenuItem.Size = New System.Drawing.Size(100, 24)
+        Me.功能选择SToolStripMenuItem.Text = "功能选择(&S)"
+        '
+        '随机生成评委ToolStripMenuItem
+        '
+        Me.随机生成评委ToolStripMenuItem.Name = "随机生成评委ToolStripMenuItem"
+        Me.随机生成评委ToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.随机生成评委ToolStripMenuItem.Text = "随机生成评委"
+        '
+        '帮助HToolStripMenuItem
+        '
+        Me.帮助HToolStripMenuItem.Name = "帮助HToolStripMenuItem"
+        Me.帮助HToolStripMenuItem.Size = New System.Drawing.Size(73, 24)
+        Me.帮助HToolStripMenuItem.Text = "帮助(&H)"
+        '
+        '重启RToolStripMenuItem
+        '
+        Me.重启RToolStripMenuItem.Name = "重启RToolStripMenuItem"
+        Me.重启RToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.重启RToolStripMenuItem.Text = "重启(R)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(23.0!, 43.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(782, 553)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("黑体", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(9)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(800, 600)
+        Me.MinimumSize = New System.Drawing.Size(800, 600)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "RandomSystem"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 开始SToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 退出EToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 功能选择SToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 帮助HToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 随机生成评委ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 重启RToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ RandomSystem/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Dim a() As Integer          '评委数组
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeCombobox()      '初始化Combobox
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.Text = "" Then
+            MsgBox("请选择演讲者！！！", vbOKOnly, "警告")     '保证选择演讲者
+        Else
+            Clean()
+            GetRandomNumber()
+            ShowName()
+            CheckOut()
+        End If
+
+
+    End Sub
+    Function ProduceRandomNumber()
+        Randomize()                     '产生随机数
+        Dim x As Integer
+        x = Int(35 * Rnd() + 1)
+        Return x
+    End Function
+    Sub GetRandomNumber()               '将随机数填进评委数组a()
+        For i = 1 To 9
+            ReDim Preserve a(i)
+            Dim temp As Integer
+            temp = ProduceRandomNumber()
+            For j = 1 To UBound(a)
+                If temp <> a(j - 1) Then
+                    a(i) = temp
+                Else
+                    temp = ProduceRandomNumber()
+                    j = 0
+                End If
+            Next
+        Next
+    End Sub
+    Sub Clean()
+        ListBox1.Items.Clear()
+        ReDim a(0)
+    End Sub
+    Sub ShowName()                      '显示评委
+        Dim b(9) As JudgeName
+        For i = 1 To UBound(b)
+            b(i) = a(i)
+            ListBox1.Items.Add(b(i).ToString)
+        Next
+    End Sub
+    Enum JudgeName              '枚举评委
+        曹睿冲 = 1
+        冯文杰
+        冯晓宽
+        黄键航
+        黄亚琴
+        黄子虚
+        赖家欣
+        李秋灵
+        李伟琦
+        李耀辉
+        林培鑫
+        林译峰
+        林越
+        刘昊鑫
+        马检
+        欧阳臻
+        任杰
+        石蕴莹
+        谭海威
+        王哲轩
+        韦湖
+        吴彦辰
+        夏弋松
+        严菁菁
+        曾理
+        周诗琪
+        周毅镕
+        朱琳聪
+        祝旭乾
+        张艺
+        邓丽
+        刘清雅
+        吴桂颖
+        杨一辰
+        余锦琛
+    End Enum
+    Sub InitializeCombobox()            '初始化Combobox
+        Dim allName(35) As JudgeName
+        For i = 1 To 35
+            allName(i) = i
+            ComboBox1.Items.Add(allName(i).ToString)
+        Next
+    End Sub
+    Sub CheckOut()                      '检验是否演讲者处于评委名单
+        Dim check As Integer
+        Dim b(35) As JudgeName
+        For j = 1 To 35
+            b(j) = j
+            If ComboBox1.Text = b(j).ToString Then
+                check = j
+            End If
+        Next
+        For i = 1 To UBound(a)
+            If a(i) = check Then
+                Clean()
+                GetRandomNumber()
+                ShowName()
+                CheckOut()
+            End If
+        Next
+    End Sub
+
+    Private Sub 退出EToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 退出EToolStripMenuItem.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub 帮助HToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 帮助HToolStripMenuItem.Click
+        MsgBox("你可能按了一个假按钮！", vbOKOnly, "提示信息")
+    End Sub
+
+    Private Sub 重启RToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 重启RToolStripMenuItem.Click
+        Application.Restart()
+
+    End Sub
+End Class
